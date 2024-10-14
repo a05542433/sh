a70976b58948f3c5dc8fe5529bfa05039a630cae
a05542433 <75099753+a05542433@users.noreply.github.com>
2024-10-14 18:26:40 +0800

    Create awsauto2.sh

---
+++ awsauto2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 执行脚本1
+echo 2 | bash <(curl -Ls https://raw.githubusercontent.com/a05542433/rule_list/main/tools/tools.sh)
+
+# 执行脚本2
+S=nyanpass bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ebb2d03f-798a-45ad-8781-0a134096338d -u https://ny.nekocat.app"
