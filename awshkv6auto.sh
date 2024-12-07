f4d6e39d8e78c6fbde7cc4eff34e98c914ad0b08
a05542433 <75099753+a05542433@users.noreply.github.com>
2024-12-08 00:13:29 +0800

    Update awshkv6auto.sh

--- awshkv6auto.sh
+++ awshkv6auto.sh
@@ -6,3 +6,6 @@ echo 2 | bash <(curl -Ls https://raw.githubusercontent.com/a05542433/rule_list/m
 # 执行脚本2
 S=nyanpass bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ebb2d03f-798a-45ad-8781-0a134096338d -u https://ny.nekocat.app"
 
+# 执行脚本3
+S=nyk bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 3c7b38ed-7139-47f5-82ae-9faef452e4fb -u https://traffic.kinako.one"
+
