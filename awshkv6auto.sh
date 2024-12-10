399e7ff1206d09fd34e511d332815c9eb37fe8b2
a05542433 <75099753+a05542433@users.noreply.github.com>
2024-12-10 18:54:24 +0800

    Update awshkv6auto.sh

--- awshkv6auto.sh
+++ awshkv6auto.sh
@@ -4,11 +4,11 @@
 echo 2 | bash <(curl -Ls https://raw.githubusercontent.com/a05542433/rule_list/main/tools/tools.sh)
 
 # 执行脚本2
-S=nyanpass bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ebb2d03f-798a-45ad-8781-0a134096338d -u https://ny.nekocat.app"
+S=nyanpass bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ebb2d03f-798a-45ad-8781-0a134096338d -u https://ny.nekocat.app"
 
 # 执行脚本3
-S=nyk bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 3c7b38ed-7139-47f5-82ae-9faef452e4fb -u https://traffic.kinako.one"
+S=nyk bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 3c7b38ed-7139-47f5-82ae-9faef452e4fb -u https://traffic.kinako.one"
 
 # 执行脚本4
-S=nyzu bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 765b1aac-4391-4d30-9f98-d8a349a8eff5 -u https://zumo.moe"
+S=nyzu bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 765b1aac-4391-4d30-9f98-d8a349a8eff5 -u https://zumo.moe"
 
