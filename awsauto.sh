0fe88b2f0d106537e11d0e3bf83e9a9791ebba29
a05542433 <75099753+a05542433@users.noreply.github.com>
2024-12-10 18:53:49 +0800

    Update awsauto.sh

--- awsauto.sh
+++ awsauto.sh
@@ -4,4 +4,4 @@
 echo 2 | bash <(curl -Ls https://raw.githubusercontent.com/a05542433/rule_list/main/tools/tools.sh)
 
 # 执行脚本2
-S=nyanpass bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ebb2d03f-798a-45ad-8781-0a134096338d -u https://ny.nekocat.app"
+S=nyanpass bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ebb2d03f-798a-45ad-8781-0a134096338d -u https://ny.nekocat.app"
