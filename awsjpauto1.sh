4e9c95eee58042775d38eccb8884714134eebde2
a05542433 <75099753+a05542433@users.noreply.github.com>
2025-02-22 20:29:26 +0800

    Update awsjpauto1.sh

--- awsjpauto1.sh
+++ awsjpauto1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 执行脚本1
-echo 2 | bash <(curl -Ls https://raw.githubusercontent.com/a05542433/rule_list/main/tools/tools.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/sntpPro/rule_list/main/tools/sys.sh)
 
 # 执行脚本2
 S=nyanpass bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t a5f39c2a-b6a6-4ec0-b775-e0af45a790f2 -u https://ny.nekocat.app"
