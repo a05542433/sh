36b008d80ca6b6184cf1ba69bba120242e852860
a05542433 <75099753+a05542433@users.noreply.github.com>
2026-03-17 11:01:47 +0800

    Update awshkv6auto.sh

--- awshkv6auto.sh
+++ awshkv6auto.sh
@@ -8,3 +8,6 @@ S=nyanpass bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) 
 
 # 执行脚本3
 S=nywapp bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t adbe7fa8-e9fe-47c5-955f-4e2f9748e2ae -u https://ny.awseyun.com"
+
+# 执行脚本4
+S=nywaco bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t d8a6501d-a59b-4f53-adbf-004881b48bb5 -u https://ny.awseyun.com"
